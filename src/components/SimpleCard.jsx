021025cef1298dd7ef04b32fc7b7f60b18ba8d18
josebl1310 <josebl1310@gmail.com>
2024-04-29 20:25:03 +0200

    Feature(cards,update)

--- src/components/SimpleCard.jsx
+++ src/components/SimpleCard.jsx
@@ -1,9 +1,13 @@
-const SimpleCard = (item) => {
+const SimpleCard = ( item ) => {
     return (
-        <article className='border-2 border-gray-100 rounded-md p-2'>
-            <aside className='size-40 rounded-full bg-cover' style={{ backgroundImage:  `url(${ item.image}`}}/>
-            <h2 className='title'>{item.name}</h2>
-            <p className='description'>{item.description}</p>
+        <article className='border-2 border-blue-200 rounded-md p-4 flex items-center bg-blue-50'>
+            <div className='w-24 h-24 rounded-full overflow-hidden'>
+                <img className='w-full h-full object-cover' src={item.image} alt={item.name} />
+            </div>
+            <div className='ml-4'>
+                <h2 className='text-xl font-bold text-blue-800'>{item.name}</h2>
+                <p className='text-sm text-blue-600'>{item.description}</p>
+            </div>
         </article>
     );
 };
