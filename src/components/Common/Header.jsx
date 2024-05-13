591031a4b3a1ca44d01dbf0983b58629752f7138
David Navia <davidnaviamata@gmail.com>
2024-05-13 17:15:39 +0200

    feat(common): simplify preloader and header

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <Link className="flex title-font font-medium items-center mb-4 md:mb-0" to='/'>
           <img className="size-12" src={logo} alt="Visit Andalucía" />
-          <h1 className="ml-3 text-3xl logo">visit Andalucía</h1>
+          <h1 className="ml-3 text-3xl logo">visitandalucia.eu</h1>
         </Link>
         <Navbar />
       </div>
--- src/components/Common/Preloader.jsx
+++ src/components/Common/Preloader.jsx
@@ -1,11 +1,9 @@
-import './Preloader.css'
 
 const Preloader = () => {
     return (
         <div className="flex items-center justify-center h-screen w-100">
-            <div className="preloader-rolling"><div className="preloader">
-                <div></div>
-            </div></div>
+            <div className="relative preloader animate-spin">
+            </div>
         </div>
     )
 }
