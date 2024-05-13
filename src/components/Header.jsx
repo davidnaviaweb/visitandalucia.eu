cbe3bc4602312862878d35876e281c261ace0b4e
josebl1310 <josebl1310@gmail.com>
2024-05-13 11:46:30 +0200

    Feature(update, añadida bandera de andalucia al logo)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import DropdownMenu from './DropdownMenu'; 
-import ThemeSwitch from './ThemeSwitch';
+import React from "react";
+import DropdownMenu from "./DropdownMenu";
+import ThemeSwitch from "./ThemeSwitch";
 
 const Header = () => {
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
+          <img
+            src="src\assets\img\Bandera_de_Andalucia.svg.png"
+            alt="BanderaAndalucia"
+            className="w-10 h-10 sm:w-20 sm:h-20 text-white rounded-full border-0"
+          />
+
           <span className="ml-3 text-xl">Visit Andalucía</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
