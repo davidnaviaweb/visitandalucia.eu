e9d1c8a5a4ab1aff7ead6a326769c5bc5e1000d3
josebl1310 <josebl1310@gmail.com>
2024-05-13 18:37:35 +0200

    feature(update cards)

--- src/components/Province/PlaceCardsItem.jsx
+++ src/components/Province/PlaceCardsItem.jsx
@@ -3,13 +3,13 @@ const PlaceCardsItems = (item) => {
     return (
         <div className="flex flex-col shadow-xl rounded-2xl">
             <figure className="flex items-center justify-center rounded-2xl">
-                <img src="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png" alt="Card Preview" className="rounded-t-2xl" />
+                <img src={item.image} alt="Card Preview" className="rounded-t-2xl" style={{ width: '450px', height: '350px' }}/>
             </figure>
             <div className="flex flex-col p-8">
-                <div className="text-2xl font-bold   text-[#374151] pb-6" >Generator</div>
-                <div className=" text-lg   text-[#374151]">Leverage a graphical editor to create beautiful web components.</div>
+                <div className="text-2xl font-bold   text-[#374151] pb-6" >{item.name}</div>
+                <div className=" text-lg   text-[#374151]">{item.territories[0].name}</div>
                 <div className="flex justify-end pt-6">
-                    <button className="bg-[#7e22ce] text-[#ffffff]  font-bold text-base  p-3 rounded-lg hover:bg-purple-800 active:scale-95 transition-transform transform" >Try it out!</button>
+                    <button className="bg-[#7e22ce] text-[#ffffff]  font-bold text-base  p-3 rounded-lg hover:bg-purple-800 active:scale-95 transition-transform transform" >Visitame!</button>
                 </div>
             </div>
         </div>
