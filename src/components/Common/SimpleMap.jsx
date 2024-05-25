51d76f86518cfaba3c875eaac5c932ba1ed2f74d
David Navia <davidnaviamata@gmail.com>
2024-05-25 23:47:14 +0200

    fix(map): don't show the map if there are no places

--- src/components/Common/SimpleMap.jsx
+++ src/components/Common/SimpleMap.jsx
@@ -16,18 +16,19 @@ const mapboxToken = 'pk.eyJ1IjoiZGF2aWRuYXZpYXdlYiIsImEiOiJjbHYyb2xqNDUwYXFnMmtw
 
 function SimpleMap({ places, center, zoom, width, height, popup }) {
     const [popupInfo, setPopupInfo] = useState(null);
-    
+
     if (center == undefined) {
         center = { latitude: 37.2147497, longitude: -4.6595196 }
     }
-    if(zoom == undefined) { 
-        zoom= 6.5
+
+    if (zoom == undefined) {
+        zoom = 6.5
     }
 
-    const newPlaces = places.filter(place => place.x_coord && place.y_coord);
-    
+    const newPlaces = places.filter(place => (place.x_coord && place.y_coord));
+    const noPlaces = newPlaces.length == 0;
+
     const pins = newPlaces.map((marker, index) => (
-        
         <Marker
             key={`marker-${index}`}
             latitude={marker.y_coord}
@@ -37,7 +38,7 @@ function SimpleMap({ places, center, zoom, width, height, popup }) {
                 // If we let the click event propagates to the map, it will immediately close the popup
                 // with `closeOnClick: true`
                 e.originalEvent.stopPropagation();
-                if(popup)
+                if (popup)
                     setPopupInfo(marker);
             }}
         >
@@ -47,37 +48,42 @@ function SimpleMap({ places, center, zoom, width, height, popup }) {
 
     return (
         <>
-            <Map
-                mapboxAccessToken={mapboxToken}
-                initialViewState={{
-                    latitude: center.latitude,
-                    longitude: center.longitude,
-                    zoom: zoom,
-                    bearing: 0,
-                    pitch: 0
-                }}
-                style={{ width: width || '100%', height: height || 600, borderRadius: 20, overflow: 'hidden' }}
-                mapStyle="mapbox://styles/mapbox/streets-v10"
-            >
-                <GeolocateControl position="top-left" />
-                <NavigationControl position="top-right" />
-                <ScaleControl position="bottom-right" />
-                {pins}
-                {popupInfo && (
-                    <Popup
-                        anchor="top"
-                        longitude={Number(popupInfo.x_coord)}
-                        latitude={Number(popupInfo.y_coord)}
-                        onClose={() => setPopupInfo(null)}
-                        className='shadow-xl'
-                    >
-                        <div className='flex flex-col w-full h-full'>
-                            <img src={popupInfo.image} alt={popupInfo.name} className="object-cover rounded-sm aspect-video" />
-                            <Link to={`/lugar/${popupInfo.slug}`} className='mt-2 text-sm'>{popupInfo.name}</Link>
-                        </div>
-                    </Popup>
-                )}
-            </Map>
+            {noPlaces ? (
+                <>
+                </>
+            ) : (
+                <Map
+                    mapboxAccessToken={mapboxToken}
+                    initialViewState={{
+                        latitude: center.latitude,
+                        longitude: center.longitude,
+                        zoom: zoom,
+                        bearing: 0,
+                        pitch: 0
+                    }}
+                    style={{ width: width || '100%', height: height || 600, borderRadius: 20, overflow: 'hidden' }}
+                    mapStyle="mapbox://styles/mapbox/streets-v10"
+                >
+                    <GeolocateControl position="top-left" />
+                    <NavigationControl position="top-right" />
+                    <ScaleControl position="bottom-right" />
+                    {pins}
+                    {popupInfo && (
+                        <Popup
+                            anchor="top"
+                            longitude={Number(popupInfo.x_coord)}
+                            latitude={Number(popupInfo.y_coord)}
+                            onClose={() => setPopupInfo(null)}
+                            className='shadow-xl'
+                        >
+                            <div className='flex flex-col w-full h-full'>
+                                <img src={popupInfo.image} alt={popupInfo.name} className="object-cover rounded-sm aspect-video" />
+                                <Link to={`/lugar/${popupInfo.slug}`} className='mt-2 text-sm'>{popupInfo.name}</Link>
+                            </div>
+                        </Popup>
+                    )}
+                </Map>
+            )}
         </>
     );
 }
