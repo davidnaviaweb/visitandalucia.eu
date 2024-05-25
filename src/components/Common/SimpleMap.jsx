2d31519e8128f3f27a19d66d4c96cbc22fe5e686
David Navia <davidnaviamata@gmail.com>
2024-05-25 23:48:13 +0200

    fix(map): revert don't show the map if there are no places

--- src/components/Common/SimpleMap.jsx
+++ src/components/Common/SimpleMap.jsx
@@ -47,44 +47,38 @@ function SimpleMap({ places, center, zoom, width, height, popup }) {
     ));
 
     return (
-        <>
-            {noPlaces ? (
-                <>
-                </>
-            ) : (
-                <Map
-                    mapboxAccessToken={mapboxToken}
-                    initialViewState={{
-                        latitude: center.latitude,
-                        longitude: center.longitude,
-                        zoom: zoom,
-                        bearing: 0,
-                        pitch: 0
-                    }}
-                    style={{ width: width || '100%', height: height || 600, borderRadius: 20, overflow: 'hidden' }}
-                    mapStyle="mapbox://styles/mapbox/streets-v10"
+
+        <Map
+            mapboxAccessToken={mapboxToken}
+            initialViewState={{
+                latitude: center.latitude,
+                longitude: center.longitude,
+                zoom: zoom,
+                bearing: 0,
+                pitch: 0
+            }}
+            style={{ width: width || '100%', height: height || 600, borderRadius: 20, overflow: 'hidden' }}
+            mapStyle="mapbox://styles/mapbox/streets-v10"
+        >
+            <GeolocateControl position="top-left" />
+            <NavigationControl position="top-right" />
+            <ScaleControl position="bottom-right" />
+            {pins}
+            {popupInfo && (
+                <Popup
+                    anchor="top"
+                    longitude={Number(popupInfo.x_coord)}
+                    latitude={Number(popupInfo.y_coord)}
+                    onClose={() => setPopupInfo(null)}
+                    className='shadow-xl'
                 >
-                    <GeolocateControl position="top-left" />
-                    <NavigationControl position="top-right" />
-                    <ScaleControl position="bottom-right" />
-                    {pins}
-                    {popupInfo && (
-                        <Popup
-                            anchor="top"
-                            longitude={Number(popupInfo.x_coord)}
-                            latitude={Number(popupInfo.y_coord)}
-                            onClose={() => setPopupInfo(null)}
-                            className='shadow-xl'
-                        >
-                            <div className='flex flex-col w-full h-full'>
-                                <img src={popupInfo.image} alt={popupInfo.name} className="object-cover rounded-sm aspect-video" />
-                                <Link to={`/lugar/${popupInfo.slug}`} className='mt-2 text-sm'>{popupInfo.name}</Link>
-                            </div>
-                        </Popup>
-                    )}
-                </Map>
+                    <div className='flex flex-col w-full h-full'>
+                        <img src={popupInfo.image} alt={popupInfo.name} className="object-cover rounded-sm aspect-video" />
+                        <Link to={`/lugar/${popupInfo.slug}`} className='mt-2 text-sm'>{popupInfo.name}</Link>
+                    </div>
+                </Popup>
             )}
-        </>
+        </Map>
     );
 }
 
