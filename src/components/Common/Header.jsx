84862883f3efe3d512c2e1f0abf8431c88ae0631
David Navia <davidnaviamata@gmail.com>
2024-05-25 20:57:34 +0200

    feat(layout): update header to be responsive

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -6,10 +6,10 @@ import logo from '../../assets/images/logo-andalucia.png';
 const Header = () => {
   return (
     <header>
-      <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
-        <Link className="flex items-center mb-4 font-medium title-font md:mb-0" to='/'>
-          <img className="size-12" src={logo} alt="Visit Andalucía" />
-          <h1 className="ml-3 text-3xl logo">visitandalucia.eu</h1>
+      <div className="container flex items-center justify-between p-5 mx-auto">
+        <Link className="flex items-start mb-4 font-medium sm:items-center title-font md:mb-0" to='/'>
+          <img className="size-8 sm:size-10 md:size-12" src={logo} alt="Visit Andalucía" />
+          <h1 className="ml-3 text-lg sm:text-xl md:text-2xl logo">visitandalucia.eu</h1>
         </Link>
         <Navbar />
       </div>
